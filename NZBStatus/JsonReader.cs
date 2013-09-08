a8ff8be17807aa5621e8eac244e8ca88ee988733
GunmanTheH <4093289+gunmantheh@users.noreply.github.com>
2013-09-09 00:36:46 +0200

    Initial commit

---
+++ NZBStatus/JsonReader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace NZBStatus
+{
+    public class JsonReader
+    {
+        private readonly string _url;
+        private readonly string _apiKey;
+        private JToken _jsonString;
+        private JArray _slots;
+        private readonly WebClient _webClient;
+
+        public decimal TotalMB
+        {
+            get
+            {
+                return GetRootValue<decimal>("mb");
+            }
+        }
+
+        public decimal AlreadyDownloadedMB
+        {
+            get
+            {
+                return GetRootValue<decimal>("mb") - GetRootValue<decimal>("mbleft");
+            }
+        }
+
+        private TClassType GetRootValue<TClassType>(string key)
+        {
+            var token = _jsonString.SelectToken(key);
+            var value = token != null ? token.Value<TClassType>() : default(TClassType);
+            return value;
+        }
+
+        public JsonReader(string url, string apiKey)
+        {
+            _url = url;
+            _apiKey = apiKey;
+            _webClient = new WebClient();
+            InitializeData();
+        }
+
+        private void InitializeData()
+        {
+            _jsonString = JObject.Parse(GetData()).GetValue("queue");
+            _slots = (JArray)_jsonString["slots"];
+        }
+
+        private string GetData()
+        {
+            return _webClient.DownloadString(string.Format("{0}&apikey={1}", _url, _apiKey));
+        }
+
+    }
+}
---
+++ NZBStatus/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace NZBStatus
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonReader = new JsonReader("http://hydra:8080/sabnzbd/api?mode=queue&output=json",
+                                            "0d40686633a53a316e0e9d46020a98c5");
+            Console.Clear();
+            Console.WriteLine(jsonReader.TotalMB);
+            Console.Title = String.Format("SabNZBd watcher: {0}/{1}", jsonReader.AlreadyDownloadedMB, jsonReader.TotalMB);
+        }
+    }
+}
